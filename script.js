ff9b65d66a164012d3b1ae83416d39db0026f1f7
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-16 19:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let time = document.getElementById("timer");
+const CurrentDate = new Date();
+const year = CurrentDate.getFullYear();
+const month = CurrentDate.getMonth() + 1;
+const Day = CurrentDate.getDay();
+let hours = CurrentDate.getHours();
+const minute = CurrentDate.getMinutes();
+const second = CurrentDate.getSeconds();
+let TimeSatmp = "";
+if(hours>12){
+hours=hours%12;
+TimeSatmp = "PM";
+}
+else{
+  TimeSatmp = "AM";
+}
+DtTime = `${month}/${Day}/${year}, ${hours}:${minute}:${second} ${TimeSatmp}`
+time.innerHTML = DtTime;
